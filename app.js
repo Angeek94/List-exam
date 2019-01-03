c9d67e8c3cb30a7d3ce1a92286c63f1991f4896d
Angeek94 <39496533+Angeek94@users.noreply.github.com>
2019-01-03 16:10:39 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 var list=[];
 var users=[];
 var readUsers=[];
+var statusUsers=[];
 var counter=0;
+
 exports.reset=function(){
     list=[];
 }
+
 exports.insertToDo=function(name,description,completed,user){
-    if(user=="tizio"||user=="caio"||user=="sempronio"){
-    list.push({
+  
+   ToDo= {
         id:counter++, 
         name: name,
         description: description,
         completed:completed ,
         assignedTo: user
-    })}
+    }
+    if(user.toLowerCase()=="tizio"||user.toLowerCase()=="caio"||user.toLowerCase()=="sempronio"){
+        list.push(ToDo);
+    }
    
 return list;
 }
@@ -26,48 +32,62 @@ exports.deleteToDo=function(idDelete){
     for(var i=0;i<list.length;i++){
         if(list[i].id===idDelete){
           
-             list.splice(i,1);
+            list.splice(i,1);
         }
     }
   return list;
 }
 
-exports. modifyStatusfromTruetoFalse=function(id){
+exports. modifyStatus=function(id){
     for(var i=0;i<list.length;i++){
-                 if(list[i].id==id){
-                   if(list[i].completed==true){
+                if(list[i].id==id){
+                    if(list[i].completed==true){
                        list[i].completed=false;
-                   }
+
+                    }else if(list[i].completed==false){
+                        list[i].completed=true;
+                    }
                 }
                  
     }
  }
 
- exports. modifyStatusfromFalsetoTrue=function(id){
-    for(var i=0;i<list.length;i++){
-                 if(list[i].id==id){
-                   if(list[i].completed==false){
-                       list[i].completed=true;
-                   }
-                }
-                 
-    }
- }
+ 
 exports.readListByUser=function(user){
     
     var j=0;
     for(var i=0;i<list.length;i++){
-      
+    
         if(list[i].assignedTo==user){
-            
-           readUsers[j]=list[i];
-           j++;
+            readUsers[j]=list[i];
+            j++;
         }
-       
+    }
+return readUsers;
+}
+exports.readListByStatus=function(status){
+    
+    var j=0;
+    for(var i=0;i<list.length;i++){
+
+        if(list[i].completed.toString()===status){
+            statusUsers[j]=list[i];
+            j++;
+        }
+    }
+return statusUsers;
+}
+exports.modifyToDoById=function(id,description){
+  
+ 
+    for(var i=0;i<list.length;i++){
+        if(list[i].id==id){
+            list[i].description=description;
+        }
 
     }
-    return readUsers;
-  }
+    return list;
+}
 exports.getList=function(){
     return list;
 }
